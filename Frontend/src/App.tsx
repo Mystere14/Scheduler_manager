05e88d466d1a9f670dd2665b39ededbe843aea6d
MrNicolas.dot <nicolas.ancelet@etudiant.univ-lr.com>
2026-05-13 14:09:27 +0200

    feat(Front): création d'une page d'accueil

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css'
+import { useState } from 'react'
+import { WelcomePage } from './page/WelcomePage/WelcomePage'
 import { SchedulerPage } from './page/SchedulerPage/SchedulerPage'
 
 function App() {
-  return <SchedulerPage />
+  const [currentPage, setCurrentPage] = useState<'welcome' | 'scheduler'>('welcome')
+
+  const handleNavigateToScheduler = () => {
+    setCurrentPage('scheduler')
+  }
+
+  return (
+    <>
+      {currentPage === 'welcome' && (
+        <WelcomePage onNavigateToScheduler={handleNavigateToScheduler} />
+      )}
+      {currentPage === 'scheduler' && (
+        <SchedulerPage />
+      )}
+    </>
+  )
 }
 
 export default App
